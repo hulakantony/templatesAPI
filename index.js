e4d33b4e92352e5773ab2e313adb4f57c165a1ff
Anton <hulak.antony1992@gmail.com>
2017-11-06 16:58:43 +0200

    add filters and search functionality

--- index.js
+++ index.js
@@ -9,17 +9,38 @@ const app = express();
 let db;
 const MongoClient = mongodb.MongoClient;
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(morgan('dev'));
 
-const router = express.Router();
 const SERVER_PORT = 8080;
 
 app.get('/templates', (req, res) => {
-  db.collection('templates').find().toArray(function(err, results) {
-    res.json(results);
-  });
+  const isPrivate = req.query.privacy === 'private';
+  const skipSize = parseInt(req.query.offset) || 0;
+  const filters = (!req.query.filters || req.query.filters === 'undefined') ? [] : req.query.filters.split('_');
+  const searchValue = (!req.query.search || req.query.search === 'undefined') ? '' : req.query.search;
+  const regex = searchValue ? new RegExp(searchValue, 'ig') : new RegExp('.');
+  if (filters.length) {
+    db.collection('templates')
+    .find({ private: isPrivate, nesting: { '$in': filters }, name: regex })
+    .sort({ createdAt: -1 })
+    .limit(10)
+    .skip(skipSize)
+    .toArray(function(err, results) {
+      res.json(results);
+    });
+  } else {
+    db.collection('templates')
+    .find({ private: isPrivate, name: regex })
+    .sort({ createdAt: -1 })
+    .limit(10)
+    .skip(skipSize)
+    .toArray(function(err, results) {
+      res.json(results);
+    });
+  }
+
 });
 
 app.post('/templates', (req, res, next) => {
