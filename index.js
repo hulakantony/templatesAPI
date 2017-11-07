d232cdeb980be7742e2a7a2f6710e067f8c23860
Anton <hulak.antony1992@gmail.com>
2017-11-07 14:48:10 +0200

    add error handler

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongodb from 'mongodb';
 import morgan from 'morgan';
 import cors from 'cors' ;
+import errorHandler from './middlewares/errorHandler';
 import { config } from './config';
 
 const app = express();
@@ -15,43 +16,65 @@ app.use(morgan('dev'));
 
 const SERVER_PORT = 8080;
 
-app.get('/templates', (req, res) => {
+app.get('/templates', async (req, res, next) => {
   const isPrivate = req.query.privacy === 'private';
   const skipSize = parseInt(req.query.offset) || 0;
   const filters = (!req.query.filters || req.query.filters === 'undefined') ? [] : req.query.filters.split('_');
   const searchValue = (!req.query.search || req.query.search === 'undefined') ? '' : req.query.search;
   const regex = searchValue ? new RegExp(searchValue, 'ig') : new RegExp('.');
+  let templates;
   if (filters.length) {
-    db.collection('templates')
-    .find({ private: isPrivate, nesting: { '$in': filters }, name: regex })
-    .sort({ createdAt: -1 })
-    .limit(10)
-    .skip(skipSize)
-    .toArray(function(err, results) {
-      res.json(results);
-    });
-  } else {
-    db.collection('templates')
-    .find({ private: isPrivate, name: regex })
-    .sort({ createdAt: -1 })
-    .limit(10)
-    .skip(skipSize)
-    .toArray(function(err, results) {
-      res.json(results);
-    });
-  }
+    try {
+      templates = await db.collection('templates')
+      .find({ private: isPrivate, nesting: { '$in': filters }, name: regex })
+      .sort({ createdAt: -1 })
+      .limit(10)
+      .skip(skipSize)
+    } catch (err) {
+      next({
+        status: 404,
+        message: 'Sorry, some troubls with your request'
+      })
+    }
 
+  } else {
+    try {
+      templates = await db.collection('templates')
+      .find({ private: isPrivate, name: regex })
+      .sort({ createdAt: -1 })
+      .limit(10)
+      .skip(skipSize)
+    } catch (err) {
+      next({
+        status: 404,
+        message: 'Sorry, some troubls with your request'
+      })
+    }
+  }
+  templates.toArray(function(err, results) {
+    if (err) {
+      next( {
+        status: 400,
+        message: 'Not posible to prepare your query'
+      })
+    }
+    res.json(results);
+  })
 });
 
 app.post('/templates', (req, res, next) => {
   db.collection('templates').save(req.body, (err, result) => {
     if (err) {
-      next(err.message);
+      next({
+        status: 404,
+        message: 'Problems to save your template'
+      });
     }
-    console.log('template saved');
     res.json('saved');
   })
 });
+
+app.use(errorHandler);
 
 MongoClient.connect(config.db, (err, database) => {
   if (err) {
